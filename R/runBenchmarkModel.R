008418ff09792e8420730fc0cdc4463b70f12b62
Ross Williams <rwilliams@ipci.nl>
2023-09-15 13:12:19 +0200

    initial package creation inc cover, mncs, hfint2dm

---
+++ R/runBenchmarkModel.R
@@ -0,0 +1,132 @@
+#' Trains one of the benchmark prediction problem with standard settingsfor a Lasso LR model
+#'
+#' @details
+#' The user specifies the target and outcome pair as well as the TAR. Combining this with connection details and pre instantiated cohorts allows for the running of the benchmark model
+#'
+#' @param targetId The targetId of the cohort, this is available in the cohort_table of the database where the cohorts were instantiated 
+#' @param outcomeId The outcomeId of the cohort, this is available in the cohort_table of the database where the cohorts were instantiate
+#' @param analysisId The analysisId which matches the benchmark problem
+#' @param analysisName the analysisName which matches the benchmark problem
+#' @param riskWindowStart The start of the time at risk period 
+#' @param riskWindowEnd The end of the time at risk period
+#' @param connectionDetails databaseconnector connectionDetails object
+#' @param cdmDatabaseSchema the name of the table where the cdm is found in the dtaabase
+#' @param cohortDatabaseSchema a writable table where the cohorts are found
+#' @param cohortTable the name of the table where the cohorts are stored
+#'
+#' @return
+#' The trained benchmark model
+#'
+#' @export
+runBenchmarkModel <- function(targetId, 
+                              outcomeId, 
+                              analysisId,
+                              analysisName,
+                              riskWindowStart,
+                              riskWindowEnd,
+                              connectionDetails, 
+                              cdmDatabaseSchema, 
+                              cohortDatabaseSchema,
+                              cohortTable,
+                              saveModel = FALSE){
+  # Select cohorts
+  cohortNames <- cohortsToCreate$cohortName[targetId]
+  outcomeNames <- cohortsToCreate$cohortName[outcomeId]
+  
+  covariateSettings <- FeatureExtraction::createCovariateSettings(useDemographicsGender = TRUE,
+                                                                  useDemographicsAge = TRUE,                                                                  useConditionGroupEraLongTerm = TRUE,
+                                                                  useConditionEraLongTerm = TRUE,
+                                                                  useConditionEraMediumTerm = TRUE,
+                                                                  useConditionEraShortTerm = TRUE,
+                                                                  useDrugEraLongTerm = TRUE,
+                                                                  useDrugEraMediumTerm = TRUE,
+                                                                  useDrugEraShortTerm = TRUE,
+                                                                  useProcedureOccurrenceLongTerm = TRUE,
+                                                                  useProcedureOccurrenceMediumTerm = TRUE,
+                                                                  useProcedureOccurrenceShortTerm = TRUE,
+                                                                  useObservationLongTerm = TRUE,
+                                                                  useObservationMediumTerm = TRUE,
+                                                                  useObservationShortTerm = TRUE,
+                                                                  longTermStartDays = -365,
+                                                                  endDays = -1)
+  
+  databaseDetails <- PatientLevelPrediction::createDatabaseDetails(
+    connectionDetails = connectionDetails,
+    cdmDatabaseSchema = cdmDatabaseSchema,
+    cdmDatabaseName = '',
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable,
+    targetId = targetId,
+    outcomeDatabaseSchema = cohortDatabaseSchema,
+    outcomeTable = cohortTable,
+    outcomeIds = outcomeId,
+    cdmVersion = 5
+  )
+  
+  
+  restrictPlpDataSettings <- PatientLevelPrediction::createRestrictPlpDataSettings()
+  
+  plpData <- PatientLevelPrediction::getPlpData(
+    databaseDetails = databaseDetails, 
+    covariateSettings = covariateSettings,
+    restrictPlpDataSettings = restrictPlpDataSettings
+  )
+  populationSettings <- PatientLevelPrediction::createStudyPopulationSettings(
+    washoutPeriod = 1095,
+    firstExposureOnly = FALSE,
+    removeSubjectsWithPriorOutcome = FALSE,
+    priorOutcomeLookback = 1,
+    riskWindowStart = riskWindowStart,
+    riskWindowEnd = riskWindowEnd,
+    startAnchor =  'cohort start',
+    endAnchor =  'cohort start',
+    minTimeAtRisk = 364,
+    requireTimeAtRisk = TRUE,
+    includeAllOutcomes = TRUE
+  )
+  
+  splitSettings <- PatientLevelPrediction::createDefaultSplitSetting(
+    trainFraction = 0.75,
+    testFraction = 0.25,
+    type = 'stratified',
+    nfold = 2, 
+    splitSeed = 1234
+  )
+  
+  preprocessSettings <- PatientLevelPrediction::createPreprocessSettings(
+    minFraction = 0.01, 
+    normalize = T, 
+    removeRedundancy = T
+  )
+  
+  sampleSettings <- PatientLevelPrediction::createSampleSettings()
+  
+  lrModel <- PatientLevelPrediction::setLassoLogisticRegression()
+  
+  lrResults <- PatientLevelPrediction::runPlp(
+    plpData = plpData,
+    outcomeId = outcomeId, 
+    analysisId = analysisId,
+    analysisName = analysisName,
+    populationSettings = populationSettings, 
+    splitSettings = splitSettings,
+    sampleSettings = sampleSettings, 
+    # featureEngineeringSettings = featureEngineeringSettings, 
+    preprocessSettings = preprocessSettings,
+    modelSettings = lrModel,
+    logSettings = PatientLevelPrediction::createLogSettings(), 
+    executeSettings = PatientLevelPrediction::createExecuteSettings(
+    runSplitData = T, 
+    runSampleData = T, 
+    runfeatureEngineering = T, 
+    runPreprocessData = T, 
+    runModelDevelopment = T, 
+    runCovariateSummary = T
+    ), 
+    saveDirectory = file.path(getwd(), 'singlePlp')
+  )
+  
+  if(!saveModel){
+    PatientLevelPrediction::savePlpModel(lrResults$model, dirPath = file.path(getwd(), "model"))
+  }
+}
---
+++ extras/CodeToRun.R
@@ -0,0 +1,138 @@
+#User specified input
+cdmDatabaseSchema = ""
+cohortDatabaseSchema = ""
+cdmDatabaseName = ""
+
+dbms = "postgresql"
+user = ""
+pw = ""
+server = ""
+port = 5432
+  
+cohortTable = "benchmark_models"
+
+#choose which benchmarks to run
+runHfint2dm <- TRUE
+runCoverModels <- TRUE
+runRCRI <- TRUE
+
+
+
+#Do not edit 
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms <- dbms,
+                                                                user <- user,
+                                                                pw <- pw,
+                                                                server <- server,
+                                                                port = port)
+
+# Create cohorts
+cohortsToCreate <- CohortGenerator::createEmptyCohortDefinitionSet()
+cohortJsonFiles <- list.files("./inst/cohorts", full.names = TRUE)
+for (i in 1:length(cohortJsonFiles)) {
+  cohortJsonFileName <- cohortJsonFiles[i]
+  cohortName <- tools::file_path_sans_ext(basename(cohortJsonFileName))
+  cohortJson <- readChar(cohortJsonFileName, file.info(cohortJsonFileName)$size)
+  cohortExpression <- CirceR::cohortExpressionFromJson(cohortJson)
+  cohortSql <- CirceR::buildCohortQuery(cohortExpression, options = CirceR::createGenerateOptions(generateStats = FALSE))
+  cohortsToCreate <- rbind(cohortsToCreate, data.frame(cohortId = i,
+                                                       cohortName = cohortName, 
+                                                       sql = cohortSql,
+                                                       stringsAsFactors = FALSE))
+}
+
+# Create the cohort tables to hold the cohort generation results
+cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = cohortTable)
+CohortGenerator::createCohortTables(connectionDetails = connectionDetails,
+                                    cohortDatabaseSchema = cohortDatabaseSchema,
+                                    cohortTableNames = cohortTableNames)
+# Generate the cohorts
+cohortsGenerated <- CohortGenerator::generateCohortSet(connectionDetails = connectionDetails,
+                                                       cdmDatabaseSchema = cdmDatabaseSchema,
+                                                       cohortDatabaseSchema = cohortDatabaseSchema,
+                                                       cohortTableNames = cohortTableNames,
+                                                       cohortDefinitionSet = cohortsToCreate)
+
+# Get the cohort counts
+cohortCounts <- CohortGenerator::getCohortCounts(connectionDetails = connectionDetails,
+                                                 cohortDatabaseSchema = cohortDatabaseSchema,
+                                                 cohortTable = cohortTableNames$cohortTable)
+print(cohortCounts)
+
+### run diabetes heartfailure benchmark
+if(runHfint2dm){
+  runBenchmarkModel(targetId = 2,
+                    outcomeId = 1,
+                    analysisId = 1000,
+                    analysisName = "hfint2dm", 
+                    riskWindowStart = 1,
+                    riskWindowEnd = 365,
+                    connectionDetails = connectionDetails,
+                    cdmDatabaseSchema = cdmDatabaseSchema,
+                    cohortDatabaseSchema = cohortDatabaseSchema,
+                    cohortTable = cohortTable
+                    )
+}
+
+if(runCoverModels){
+  runBenchmarkModel(targetId = 7,
+                    outcomeId = 8,
+                    analysisId = 1001,
+                    analysisName = "coverF", 
+                    riskWindowStart = 1,
+                    riskWindowEnd = 30,
+                    connectionDetails = connectionDetails,
+                    cdmDatabaseSchema = cdmDatabaseSchema,
+                    cohortDatabaseSchema = cohortDatabaseSchema,
+                    cohortTable = cohortTable
+  )
+  
+  runBenchmarkModel(targetId = 7,
+                    outcomeId = 9,
+                    analysisId = 1002,
+                    analysisName = "coverI", 
+                    riskWindowStart = 1,
+                    riskWindowEnd = 30,
+                    connectionDetails = connectionDetails,
+                    cdmDatabaseSchema = cdmDatabaseSchema,
+                    cohortDatabaseSchema = cohortDatabaseSchema,
+                    cohortTable = cohortTable
+  )
+  
+  runBenchmarkModel(targetId = 7,
+                    outcomeId = 10,
+                    analysisId = 1003,
+                    analysisName = "coverH", 
+                    riskWindowStart = 1,
+                    riskWindowEnd = 30,
+                    connectionDetails = connectionDetails,
+                    cdmDatabaseSchema = cdmDatabaseSchema,
+                    cohortDatabaseSchema = cohortDatabaseSchema,
+                    cohortTable = cohortTable
+  )
+ }  
+
+if(runRCRI){
+  runBenchmarkModel(targetId = 3,
+                    outcomeId = 5,
+                    analysisId = 1004,
+                    analysisName = "MNCSwithCreatine", 
+                    riskWindowStart = 1,
+                    riskWindowEnd = 30,
+                    connectionDetails = connectionDetails,
+                    cdmDatabaseSchema = cdmDatabaseSchema,
+                    cohortDatabaseSchema = cohortDatabaseSchema,
+                    cohortTable = cohortTable
+  )
+  runBenchmarkModel(targetId = 4,
+                    outcomeId = 5,
+                    analysisId = 1005,
+                    analysisName = "MNCSwithoutCreatine", 
+                    riskWindowStart = 1,
+                    riskWindowEnd = 30,
+                    connectionDetails = connectionDetails,
+                    cdmDatabaseSchema = cdmDatabaseSchema,
+                    cohortDatabaseSchema = cohortDatabaseSchema,
+                    cohortTable = cohortTable
+  )
+
+}
