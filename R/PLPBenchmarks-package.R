afbaf91da4ae3374b0e4844f118e938e9417a3aa
Solon Ioannou <sioann88@gmail.com>
2025-12-11 15:21:55 +0100

    Adding package .R file

--- R/PLPBenchmarks-package.R
+++ R/PLPBenchmarks-package.R
@@ -2,6 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr .data
+#' @importFrom kableExtra kable_classic
+#' @importFrom kableExtra kable_paper
+#' @importFrom kableExtra scroll_box
 #' @importFrom tidyr pivot_wider
 #' @importFrom utils read.csv
 #' @importFrom utils write.csv
