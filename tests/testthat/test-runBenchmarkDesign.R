2323772708ebd425eac313b0b18b2260bd1fe996
Solon Ioannou <sioann88@gmail.com>
2025-12-14 15:20:31 +0100

    test improvements

--- tests/testthat/test-runBenchmarkDesign.R
+++ tests/testthat/test-runBenchmarkDesign.R
@@ -9,6 +9,11 @@ test_that("runs without population creation", {
   
   designNames <- names(eunomiaBenchmarkDesign_1)
   expect_no_error(runBenchmarkDesign(benchmarkDesign = eunomiaBenchmarkDesign_1)) 
+  
+  suppressWarnings({
+    runBenchmarkDesign(benchmarkDesign = eunomiaBenchmarkDesign_1)
+  })
+  
   expect_equal(as.vector(sapply(eunomiaBenchmarkDesign_1, "[[", "saveDirectory")), file.path(saveDirectory, "eunomia_design_1", designNames))
   expect_directory(x = file.path(saveDirectory, "eunomia_design_1", designNames, "plpResult"))
   expect_file(x = file.path(saveDirectory, "eunomia_design_1", designNames, "plpResult", "runPlp.Rds"))
